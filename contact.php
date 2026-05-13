8ee437843eccad69734962db4ae3ec25d3d6873e
DanPriven <150908743+DanPriven@users.noreply.github.com>
2026-05-13 23:50:00 +0300

    Add contact form handling in contact.php

--- contact.php
+++ contact.php
@@ -1,1 +1,13 @@
+<?php
 
+$name = $_POST["name"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+
+echo "<h1>Contact Information</h1>";
+
+echo "Name: " . $name . "<br>";
+echo "Email: " . $email . "<br>";
+echo "Phone: " . $phone . "<br>";
+
+?>
