00fd23d655cdcf30f3fe2bd6e9e4660f2de4e4e0
DanPriven <150908743+DanPriven@users.noreply.github.com>
2026-05-15 03:51:04 +0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,11 +3,18 @@
 $name = $_POST["name"];
 $email = $_POST["email"];
 $phone = $_POST["phone"];
+$gender = $_POST["gender"];
+$language = $_POST["language"];
 
 echo "<h1>Contact Information</h1>";
 
 echo "Name: " . $name . "<br>";
 echo "Email: " . $email . "<br>";
 echo "Phone: " . $phone . "<br>";
+echo "Gender: " . $gender . "<br>";
+echo "Favorite Language: " . $language . "<br>";
 
+if(isset($_POST["terms"])) {
+    echo "Terms accepted";
+}
 ?>
